4a3e87063dd76dcbec96c9c2ff999d7d77b40662
KevinZuniga12 <133398094+KevinZuniga12@users.noreply.github.com>
2025-05-27 15:57:03 -0600

    [FIST COMMITE] Subida de proyecto

---
+++ src/main/java/mx/edu/utez/cvkdrz9b/Cvkdrz9bApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvkdrz9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvkdrz9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvkdrz9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/cvkdrz9b/Cvkdrz9bApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvkdrz9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvkdrz9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
