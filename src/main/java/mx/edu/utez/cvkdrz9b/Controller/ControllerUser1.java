cdc9f826b1b14a79cf7d3354d8083fe218ccf94b
KevinZuniga12 <133398094+KevinZuniga12@users.noreply.github.com>
2025-05-29 22:18:34 -0600

    [ADD] Se agregó el controlador ControllerUser1.java

---
+++ src/main/java/mx/edu/utez/cvkdrz9b/Controller/ControllerUser1.java
@@ -0,0 +1,18 @@
+package mx.edu.utez.cvkdrz9b.Controller;
+
+public class ControllerUser1 {
+
+    public void saludar() {
+        System.out.println("¡Hola! Soy el controlador de User1.");
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("User1 tiene acceso al módulo de gestión de usuarios.");
+    }
+
+    public static void main(String[] args) {
+        ControllerUser1 controlador = new ControllerUser1();
+        controlador.saludar();
+        controlador.mostrarInformacion();
+    }
+}
